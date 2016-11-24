50725d8c86643e3442a01f8da67dbf944c24c8af
nicolasribeiro <nicoribeiro@gmail.com>
2016-11-23 23:45:20 -0300

    ajustes en MessagingHelper

--- app/com/bluedot/commons/controllers/NotificationChannelController.java
+++ app/com/bluedot/commons/controllers/NotificationChannelController.java
@@ -123,7 +123,7 @@ public class NotificationChannelController extends AbstractController
 
 				notificationChannel.save();
 
-				notificationChannel.sendValidationKey(MessagingHelper.getValidationHost(request().host()));
+				notificationChannel.sendValidationKey(new MessagingHelper().getValidationHost(request().host()));
 				
 				return CompletableFuture.completedFuture(created());
 			}
@@ -197,7 +197,7 @@ public class NotificationChannelController extends AbstractController
 				if (!user.getNotificationChannels().contains(notificationChannel))
 					throw APIException.raise(APIErrors.UNAUTHORIZED).setDetailMessage("Notification Channel does not belog to you");
 
-				notificationChannel.sendValidationKey(MessagingHelper.getValidationHost(request().host()));
+				notificationChannel.sendValidationKey(new MessagingHelper().getValidationHost(request().host()));
 
 				return CompletableFuture.completedFuture(ok());
 
