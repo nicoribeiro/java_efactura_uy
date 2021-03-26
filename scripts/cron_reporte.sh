9afdbba9742738b3cfc403c0797bd83150eafdaf
rafa <qtySMLD2gXNuGpG>
2021-03-26 16:37:44 -0300

    Se separa en dos las tareas de cron. Una para reporte diario, otra para obtener resultados documentos de DGI.

--- scripts/cron_reporte.sh
+++ scripts/cron_reporte.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 DATE_START=$1
-RUT_DEFAULT_VALUE=210475270010 # especificar RUT aqui
+RUT_DEFAULT_VALUE=210475270010 # especificar RUT por omisión aqui
 RUT=${2:-$RUT_DEFAULT_VALUE}
+AUTH_TOKEN=cd0428ac-e516-417d-adbb-aa9419427d63 # especificar auth token
 PORT=9000
 BASE="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/..
 APP_NAME=java_efactura_uy
 LOGS=/var/log/srv/$APP_NAME\_cron.log
 REPORT_DATE=${DATE_START:=$(date -d "yesterday 13:00" '+%Y%m%d')}
 REPORT_URL=http://localhost:$PORT/api/v1/empresa/$RUT/reporteDiario/$REPORT_DATE?cantReportes=1
-RESULTADOS_URL=http://localhost:$PORT/api/v1/empresa/$RUT/resultados/$REPORT_DATE
-AUTH_TOKEN=cd0428ac-e516-417d-adbb-aa9419427d63
 
 #LOG functions
 LOG() {
@@ -24,9 +23,6 @@ INFO() {
 WARNING() {
   LOG "WARNING: $@"
 }
-
-INFO $RESULTADOS_URL
-curl -v -X GET "$RESULTADOS_URL" -H "AUTH-TOKEN: $AUTH_TOKEN" --compressed >>$LOGS 2>&1
 
 INFO $REPORT_URL
 curl -v -X POST "$REPORT_URL" -H "AUTH-TOKEN: $AUTH_TOKEN" --compressed >>$LOGS 2>&1
---
+++ scripts/cron_resultados.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+DATE_START=$1
+RUT_DEFAULT_VALUE=210475270010 # especificar RUT por omisión aqui
+RUT=${2:-$RUT_DEFAULT_VALUE}
+AUTH_TOKEN=cd0428ac-e516-417d-adbb-aa9419427d63 # especificar auth token
+PORT=9000
+BASE="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/..
+APP_NAME=java_efactura_uy
+LOGS=/var/log/srv/$APP_NAME\_cron.log
+REPORT_DATE=${DATE_START:=$(date '+%Y%m%d')}
+RESULTADOS_URL=http://localhost:$PORT/api/v1/empresa/$RUT/resultados/$REPORT_DATE
+
+#LOG functions
+LOG() {
+  echo "$(date):$@"
+}
+
+INFO() {
+  LOG "INFO: $@"
+}
+
+WARNING() {
+  LOG "WARNING: $@"
+}
+
+INFO $RESULTADOS_URL
+curl -v -X GET "$RESULTADOS_URL" -H "AUTH-TOKEN: $AUTH_TOKEN" --compressed >>$LOGS 2>&1
