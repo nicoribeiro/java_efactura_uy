859d6af6dd530a23b4301e4c716c61522ade5d29
rafa <qtySMLD2gXNuGpG>
2021-04-06 10:09:51 -0300

    Se vuelve a agregar llamada a endpoint resultados

--- scripts/cron_reporte.sh
+++ scripts/cron_reporte.sh
@@ -10,6 +10,7 @@ APP_NAME=java_efactura_uy
 LOGS=/var/log/srv/$APP_NAME\_cron.log
 REPORT_DATE=${DATE_START:=$(date -d "yesterday 13:00" '+%Y%m%d')}
 REPORT_URL=http://localhost:$PORT/api/v1/empresa/$RUT/reporteDiario/$REPORT_DATE?cantReportes=1
+RESULTADOS_URL=http://localhost:$PORT/api/v1/empresa/$RUT/resultados/$REPORT_DATE
 
 #LOG functions
 LOG() {
@@ -23,6 +24,9 @@ INFO() {
 WARNING() {
   LOG "WARNING: $@"
 }
+
+INFO $RESULTADOS_URL
+curl -v -X GET "$RESULTADOS_URL" -H "AUTH-TOKEN: $AUTH_TOKEN" --compressed >>$LOGS 2>&1
 
 INFO $REPORT_URL
 curl -v -X POST "$REPORT_URL" -H "AUTH-TOKEN: $AUTH_TOKEN" --compressed >>$LOGS 2>&1
